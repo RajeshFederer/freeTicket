cf7dfec5cbf5237d8209c44a5bebd78f48cdbbff
Rajeshwaran <39850@hexaware.com>
2018-01-24 15:19:07 +0530

    using default azure code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const BOT_OPEN_ID_META_DATA = '';
 const LUIS_APP_ID = '9fc3d559-6624-4900-ad38-283e0dfdbdb0';
 const LUIS_API_KEY = '7817b8e92a2f45738870896bd46edb39';
 const LUIS_API_HOST_NAME = 'westus.api.cognitive.microsoft.com';
-const LUIS_MODEL_URL = 'https://westus.api.cognitive.microsoft.com/api/v2.0/apps/9fc3d559-6624-4900-ad38-283e0dfdbdb0?subscription-key=7817b8e92a2f45738870896bd46edb39'
+const LUIS_MODEL_URL = 'https://' + LUIS_API_HOST_NAME + '/luis/v1/application?id=' + LUIS_APP_ID + '&subscription-key=' + LUIS_API_KEY;
 
 // Create chat connector for communicating with the Bot Framework Service
 const connector = new builder.ChatConnector({
